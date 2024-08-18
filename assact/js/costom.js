ab3ed153d72afc787017677f7ce878de2d1ba237
junayed-hassan <junayedhassan102045@gmail>
2024-08-18 15:54:59 +0600

    our digital clock

---
+++ assact/js/costom.js
@@ -0,0 +1,18 @@
+let hoursElement = document.getElementById("hours"); 
+let minutesElement =document.getElementById("minutes");
+let secondeElement = document.getElementById("seconde");
+let ampmElement = document.getElementById("ampm");
+
+function ourDigitalClock(){
+    let now = new Date();
+    let hoursNumber = (now.getHours()) > 12 ? (now.getHours()) - 12 : (now.getHours());
+    let minutesNumber = now.getMinutes();
+    let secondeNumber = now.getSeconds();
+    let ampmString = (now.getHours()) > 12 ? "pm" : "am";
+
+    hoursElement.innerHTML = hoursNumber < 10 ? "0" + hoursNumber : hoursNumber;
+    minutesElement.innerHTML = minutesNumber < 10 ? "0" + minutesNumber : minutesNumber;
+    secondeElement.innerHTML = secondeNumber < 10 ? "0" + secondeNumber : secondeNumber;
+    ampmElement.innerHTML = ampmString;
+}
+setInterval(ourDigitalClock, 1000)
