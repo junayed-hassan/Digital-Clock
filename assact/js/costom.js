187e5cfc9177199c8ddd48be039108b4e732455d
junayed-hassan <junayedhassan102045@gmail>
2024-08-20 19:59:44 +0600

    Digital-Clock -2

--- assact/js/costom.js
+++ assact/js/costom.js
@@ -1,18 +1,20 @@
-let hoursElement = document.getElementById("hours"); 
-let minutesElement =document.getElementById("minutes");
-let secondeElement = document.getElementById("seconde");
-let ampmElement = document.getElementById("ampm");
+let hoursElement = document.getElementById('hours');
+let minutesElement = document.getElementById('minutes');
+let secondeElement = document.getElementById('seconde');
+let ampmElement = document.getElementById('ampm');
 
-function ourDigitalClock(){
+function ourDigitalClock (){
     let now = new Date();
-    let hoursNumber = (now.getHours()) > 12 ? (now.getHours()) - 12 : (now.getHours());
+    let hoursNumber = (now.getHours()) > 12 ? (now.getHours()) -12 : (now.getHours());
     let minutesNumber = now.getMinutes();
     let secondeNumber = now.getSeconds();
-    let ampmString = (now.getHours()) > 12 ? "pm" : "am";
+    let ampmString = (now.getHours()) > 12 ? "PM" : "AM";
 
-    hoursElement.innerHTML = hoursNumber < 10 ? "0" + hoursNumber : hoursNumber;
-    minutesElement.innerHTML = minutesNumber < 10 ? "0" + minutesNumber : minutesNumber;
-    secondeElement.innerHTML = secondeNumber < 10 ? "0" + secondeNumber : secondeNumber;
+
+    hoursElement.innerHTML = (hoursNumber) < 10 ? "0" + (hoursNumber) : (hoursNumber);
+    minutesElement.innerHTML = (minutesNumber) < 10 ? "0" + (minutesNumber) : (minutesNumber) ;
+    secondeElement.innerHTML = (secondeNumber) < 10 ? "0" + (secondeNumber) : (secondeNumber) ;
     ampmElement.innerHTML = ampmString;
-}
-setInterval(ourDigitalClock, 1000)
+} 
+
+setInterval(ourDigitalClock, 1000);
